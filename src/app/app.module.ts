02e407ce4c653836d73ee059efa9756ec61aae24
runskmr <runskmr14@gmail.com>
2023-06-01 14:07:24 -0400

    Initial Boilerplate

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { ContactModule } from './contact/contact.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, ContactModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
